290cb90ecabe418ef6ef50ec52bf8509c4f8ddbf
bingbinglai <imbingbinglai@gmail.com>
2018-07-29 18:34:23 -0400

    while

--- model.py
+++ model.py
@@ -42,44 +42,45 @@ def _generator(samples, batch_size=32):
     # ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
 
     num_samples = len(samples)
-    print('hi in generator')
 
-    shuffle(samples)
-    for offset in range(0, num_samples, batch_size):
-        batch_samples = samples[offset:offset+batch_size]
+    while 1:
+        print('hi in generator')
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-        images = []
-        angles = []
-        for batch_sample in batch_samples:
-            # center
-            center = batch_sample[0]
-            directory = center.split('/')[-3]
-            image_path = center.split('/')[-1]
-            name = './{}/IMG/{}'.format(directory, image_path)
-            center_image = cv2.imread(name)
-            center_angle = float(batch_sample[3])
-            images.append(center_image)
-            angles.append(center_angle)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                # center
+                center = batch_sample[0]
+                directory = center.split('/')[-3]
+                image_path = center.split('/')[-1]
+                name = './{}/IMG/{}'.format(directory, image_path)
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
 
-            # left
-            left = batch_sample[1]
-            directory = left.split('/')[-3]
-            image_path = left.split('/')[-1]
-            name = './{}/IMG/{}'.format(directory, image_path)
-            left_image = cv2.imread(name)
-            left_angle = float(batch_sample[3])
-            images.append(left_image)
-            angles.append(left_angle + _CORRECTION_NUM)
+                # left
+                left = batch_sample[1]
+                directory = left.split('/')[-3]
+                image_path = left.split('/')[-1]
+                name = './{}/IMG/{}'.format(directory, image_path)
+                left_image = cv2.imread(name)
+                left_angle = float(batch_sample[3])
+                images.append(left_image)
+                angles.append(left_angle + _CORRECTION_NUM)
 
-            # right
-            right = batch_sample[2]
-            directory = right.split('/')[-3]
-            image_path = right.split('/')[-1]
-            name = './{}/IMG/{}'.format(directory, image_path)
-            right_image = cv2.imread(name)
-            right_angle = float(batch_sample[3])
-            images.append(right_image)
-            angles.append(right_angle - _CORRECTION_NUM)
+                # right
+                right = batch_sample[2]
+                directory = right.split('/')[-3]
+                image_path = right.split('/')[-1]
+                name = './{}/IMG/{}'.format(directory, image_path)
+                right_image = cv2.imread(name)
+                right_angle = float(batch_sample[3])
+                images.append(right_image)
+                angles.append(right_angle - _CORRECTION_NUM)
 
 
 
