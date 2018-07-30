e0cfca585fc46b90e4f635098968d03d4c5bd7f4
bingbinglai <imbingbinglai@gmail.com>
2018-07-30 09:15:03 -0400

    this

--- model.py
+++ model.py
@@ -117,7 +117,7 @@ def train():
 
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((50,20), (0,0))))
+    model.add(Cropping2D(cropping=((64,25), (0,0))))
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
     model.add(Dropout(dropout))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
